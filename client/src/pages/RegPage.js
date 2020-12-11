bbc858977dae4acf262f0f230d3471e49ccd6a9c
ss.dantez.ss@gmail.com <ss.dantez.ss@gmail.com>
2020-12-11 22:22:05 +0300

    Implemented validation on the registration form

--- client/src/pages/RegPage.js
+++ client/src/pages/RegPage.js
@@ -1,109 +1,152 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import logo from '../images/IMG_1737.jpg'
-import {useHttp} from "../hooks/http.hook";
+import {Formik} from "formik";
+import * as yup from 'yup';
+import {NavLink} from "react-router-dom";
 
 export const RegPage = () => {
-    const {loading, error, request} = useHttp();
-    const [form, setForm] = useState({
-        email: '', password: ''
+
+    const validationsSchema = yup.object().shape({
+        lastName: yup.string()
+            .typeError('Должно быть строкой')
+            .required('Поле необходимо заполнить'),
+        firstName: yup.string()
+            .typeError('Должно быть строкой')
+            .required('Поле необходимо заполнить'),
+        email: yup.string()
+            .email('Введите верный email')
+            .required('Поле необходимо заполнить'),
+        password: yup.string()
+            .typeError('Должно быть строкой')
+            .min(8, "Пароль должен быть длиннее 8 символов")
+            .required('Поле необходимо заполнить'),
+        confirmPassword: yup.string()
+            .oneOf([yup.ref('password')], 'Пароли не совпадают')
+            .required('Поле необходимо заполнить')
     });
 
-    const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value });
-    };
-
-    const registrationHandler = async () => {
-        try {
-            const data = await request(
-                '/api/auth/registration',
-                'POST',
-                {...form});
-            console.log('Data', data);
-        } catch (e) {}
-    };
-
     return (
-        <div className="row">
-            <div className="col s12 m6 offset-m3">
-                <div className="card" style={{marginTop: 100}}>
-                    <div className="card-image">
-                        <img src={logo}/>
-                        <span className="card-title">
+        <div>
+            <Formik
+                initialValues={{
+                    lastName: '',
+                    firstName: '',
+                    patronymic: '',
+                    password: '',
+                    confirmPassword: '',
+                    email: ''
+                }}
+                validateOnBlur
+                onSubmit={values => {
+                    console.log(values) }}
+                validationSchema={validationsSchema}
+            >
+                {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty}) => (
+                    <div className="row">
+                        <div className="col s12 m6 offset-m3">
+                            <div className="card" style={{marginTop: 100}}>
+                                <div className="card-image">
+                                    <img src={logo}/>
+                                    <span className="card-title">
                                 <h3>Регистрация</h3>
                             </span>
+                                </div>
+                                <div className="card-content">
+                                    <div className="input-field">
+                                        <input
+                                            type='text'
+                                            className='red-input'
+                                            name='lastName'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.name}/>
+                                        <label htmlFor='lastName'>Фамилия *</label>
+                                        {touched.lastName && errors.lastName &&
+                                        <p className="red-text">{errors.lastName}</p>}
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            type='text'
+                                            className='red-input'
+                                            name='firstName'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.firstName}/>
+                                        <label htmlFor='firstName'>Имя *</label>
+                                        {touched.firstName && errors.firstName &&
+                                        <p className="red-text">{errors.firstName}</p>}
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            type='text'
+                                            className='red-input'
+                                            name='patronymic'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.patronymic}/>
+                                        <label htmlFor='patronymic'>Отчество</label>
+                                        {touched.patronymic && errors.patronymic &&
+                                        <p className="red-text">{errors.patronymic}</p>}
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            type='email'
+                                            className='red-input'
+                                            name='email'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.email}/>
+                                        <label htmlFor='email'>Email *</label>
+                                        {touched.email && errors.email &&
+                                        <p className="red-text">{errors.email}</p>}
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            type='password'
+                                            className='red-input'
+                                            name='password'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.password}/>
+                                        <label htmlFor='password'>Пароль *</label>
+                                        {touched.password && errors.password &&
+                                        <p className="red-text">{errors.password}</p>}
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            type='password'
+                                            className='red-input'
+                                            name='confirmPassword'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.confirmPassword}/>
+                                        <label htmlFor='confirmPassword'>Пароль еще раз *</label>
+                                        {touched.confirmPassword && errors.confirmPassword &&
+                                        <p className="red-text">{errors.confirmPassword}</p>}
+                                    </div>
+                                </div>
+                                <div className="card-action">
+                                    <button
+                                        disabled={!isValid && !dirty}
+                                        className="btn red"
+                                        style={{marginRight: 24}}
+                                        onClick={handleSubmit}
+                                        type='submit'>
+                                        Зарегистрироваться
+                                    </button>
+                                    <button
+                                        className="btn white red-text text-accent-2 z-depth-0">
+                                        <NavLink to="/"
+                                                 className="red-text text-accent-2">
+                                            Авторизация
+                                        </NavLink>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-content">
-                        <div className="input-field">
-                            <input
-                                id="lastName"
-                                type="text"
-                                name="lastName"
-                                className="red-input"
-                                onChange={changeHandler}/>
-                            <label htmlFor="lastName">Фамилия</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                id="firstName"
-                                type="text"
-                                name="firstName"
-                                className="red-input"
-                                onChange={changeHandler} />
-                            <label htmlFor="firstName">Имя</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                id="patronymic"
-                                type="text"
-                                name="patronymic"
-                                className="red-input"
-                                onChange={changeHandler} />
-                            <label htmlFor="patronymic">Отчество</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                id="email"
-                                type="email"
-                                name="email"
-                                className="red-input"
-                                onChange={changeHandler} />
-                            <label htmlFor="password">Email</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                id="password"
-                                type="password"
-                                name="password"
-                                className="red-input"
-                                onChange={changeHandler} />
-                            <label htmlFor="password">Пароль</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                id="passwordRepeated"
-                                type="password"
-                                name="passwordRepeated"
-                                className="red-input"
-                                onChange={changeHandler} />
-                            <label htmlFor="passwordRepeated">Пароль еще раз</label>
-                        </div>
-                    </div>
-                    <div className="card-action">
-                        <button
-                            className="btn red"
-                            style={{marginRight: 24}}
-                            onClick={registrationHandler}
-                            disabled={loading}>
-                            Зарегистрироваться
-                        </button>
-                        <button
-                            className="btn white red-text text-accent-2"
-                            disabled={loading} >
-                            Авторизация
-                        </button>
-                    </div>
-                </div>
-            </div>
+                )}
+            </Formik>
         </div>
     );
 };
